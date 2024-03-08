0997fa9609e1b76745a89b9e0e52525141f6ae7c
gNano <martinianogancedo@hotmail.com>
2024-03-08 14:31:34 -0300

    Index funcionality ready

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-//Contenido dinámico, seleccionador del modificador
-let modificadores = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const modificador = document.getElementById("modificadores").value;
-
-const select = document.querySelector("#modificadores");
-
-modificadores.forEach((modificador) => {
-  let modif = document.createElement("option");
-  modif.value = modificador;
-  modif.innerText = modificador;
-  select.append(modif);
-});
-
-select.addEventListener("change", () => {
-  let modif = select.options[select.selectedIndex].value;
-  console.log(modif);
-});
-
-let resultadosTiradas = []; //esto será la clave del éxito del simulador
+let savedHist = localStorage.getItem("history");
+let rollResults = savedHist ? JSON.parse(savedHist) : [];
+let lastRoll;
 
 //constructor para los objects del historial
-function tirada(dado, resultado) {
-  this.dado = dado;
-  this.resultado = resultado;
+function roll(dice, result, modifier) {
+  this.dice = dice;
+  this.result = result;
+  this.modifier = modifier;
+}
+
+//función de apoyo para mostrar el resultado
+function updateResult() {
+  const inputResult = document.getElementById("result");
+  inputResult.value = lastRoll ? lastRoll.result : "";
 }
 
 //listener para cada boton
@@ -33,37 +24,93 @@ const twelve = document.getElementById("12sides");
 const twenty = document.getElementById("20sides");
 const hundred = document.getElementById("100sides");
 
-function lanzadado(dado) {
-  let result = Math.floor(Math.random() * dado) + 1;
-  let nueva = new tirada(dado, result);
-  resultadosTiradas.push(nueva);
-  console.log(resultadosTiradas);
+function diceroller(dice) {
+  const selectedModifier = parseInt(
+    document.getElementById("modificadores").value
+  );
+  let result = Math.floor(Math.random() * dice) + 1 + selectedModifier;
+  let diceroll = new roll(dice, result, selectedModifier);
+  rollResults.push(diceroll);
+  lastRoll = rollResults[rollResults.length - 1];
+  updateResult();
+  updateHistory();
 }
 
 four.addEventListener("click", () => {
-  console.log(lanzadado(4));
+  console.log(diceroller(4));
 });
 
 six.addEventListener("click", () => {
-  console.log(lanzadado(6));
+  console.log(diceroller(6));
 });
 
 eight.addEventListener("click", () => {
-  console.log(lanzadado(8));
+  console.log(diceroller(8));
 });
 
 ten.addEventListener("click", () => {
-  console.log(lanzadado(10));
+  console.log(diceroller(10));
 });
 
 twelve.addEventListener("click", () => {
-  console.log(lanzadado(12));
+  console.log(diceroller(12));
 });
 
 twenty.addEventListener("click", () => {
-  console.log(lanzadado(20));
+  console.log(diceroller(20));
 });
 
 hundred.addEventListener("click", () => {
-  console.log(lanzadado(100));
+  console.log(diceroller(100));
 });
+
+//funciónes para el historial
+const inputHistory = document.getElementById("history");
+
+function updateHistory() {
+  if (rollResults.length == 0) {
+    inputHistory.value = "No hay historial reciente";
+  } else {
+    inputHistory.value = rollResults
+      .map((roll) => {
+        return `Lanzaste un dado de ${roll.dice} caras, con un modificador de ${roll.modifier} y el resultado final fue ${roll.result}`;
+      })
+      .join("\n");
+  }
+  inputHistory.scrollTop = inputHistory.scrollHeight;
+}
+updateHistory();
+
+//botones del historial
+const saveBtn = document.getElementById("saveHist");
+saveBtn.addEventListener("click", () => {
+  localStorage.setItem("history", JSON.stringify(rollResults));
+  updateHistory();
+});
+
+const delBtn = document.getElementById("delHist");
+delBtn.addEventListener("click", () => {
+  localStorage.removeItem("history");
+  rollResults = [];
+  updateHistory();
+});
+
+//popup tienda
+if (!localStorage.getItem("popupShown")) {
+  setTimeout(() => {
+    Swal.fire({
+      title: "¿Ya conoces nuestra tienda?",
+      showCancelButton: true,
+      confirmButtonText: "¡Visitala!",
+      cancelButtonText: "Ahora no",
+      confirmButtonColor: "green",
+      cancelButtonColor: "red",
+      icon: "question",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "html/tienda.html";
+      }
+    });
+    localStorage.setItem("popupShown", true);
+  }, 10000);
+}
